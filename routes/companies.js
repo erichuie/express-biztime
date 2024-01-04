ba4b7f0af5a3013fd0537b6b73ab3cc7478f69de
Veronica Ni <vni1294@gmail.com>
2024-01-04 15:40:58 -0500

    Implemented GET/companies/code and POST/companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const { NotFoundError } = require("../expressError");
+const { NotFoundError, BadRequestError } = require("../expressError");
 const db = require("../db");
 
 const router = express.Router();
@@ -17,9 +17,43 @@ router.get("/", async function(req, res){
   return res.json({ companies });
 });
 
+/** Return obj of company: {company: {code, name, description}}
+ *  If the company given cannot be found, returns a 404 status response.
+ */
+router.get("/:code", async function(req, res){
+  const code = req.params.code;
 
+  const results = await db.query(
+    `SELECT code, name, description
+            FROM companies
+            WHERE code = $1`, [code]);
+  
+  const company = results.rows[0];
+  if (!company) throw new NotFoundError();
+  
+  return res.json({ company });
+});
 
+/** Adds a company.
+ *  Accepts JSON like: {code, name, description}
+ *  Returns obj of new company: {company: {code, name, description}}
+ */
+router.post("/", async function(req, res){
+  if (req.body === undefined) throw new BadRequestError();
 
+  const { code, name, description } = req.body;
+  //TODO: throw error if code already exists
+  const results = await db.query(
+    `INSERT into companies (code, name, description)        
+            VALUES($1, $2, $3)
+            RETURNING code, name, description`, 
+    [code, name, description],
+  );
+  
+  const company = results.rows[0];
+  
+  return res.status(201).json({ company });
+});
 
 
 
